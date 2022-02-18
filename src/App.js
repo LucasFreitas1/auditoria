3256a7e1bdd513d3f3c7f0eb1d12c75b170ae853
Lucas Freitas de Almeida <lalmeida@brasal.com.br>
2022-02-18 17:54:17 -0300

    Montando layout inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Stack } from 'react-bootstrap';
 
 function App() {
   return (
     <html>
-        <h1>
-          Auditoria
-        </h1>
-        <h2>
-          Sistemas:
-        </h2>
-        <Button>
-          Trabalhos
-        </Button>
-        <br></br>
-        <Button>
-          Apontamentos
-        </Button>
-  
+      <div>
+        
+      <Container>
+            <h1>
+            Auditoria
+            </h1>
+        <Row> 
+         {/* Primeira coluna com botões */}
+          <Col xs={3}>
+            <Stack gap = {3}>
+              <Button>
+              Trabalhos
+              </Button>
+              <Button>
+              Apontamentos
+              </Button>
+            </Stack>
+          </Col>
+         {/* Segunda coluna com informações relevantes */}
+          <Col xs={6}>
+          Teste de informações
+          </Col>
+        </Row>
+      </Container>
+      </div>
   </html>
 
   );
