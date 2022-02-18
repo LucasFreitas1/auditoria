d0eeea372bcf0a840442ae752391deecb31a2326
Lucas Freitas de Almeida <lalmeida@brasal.com.br>
2022-02-18 11:32:49 -0300

    Inclusão bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-  <div>
-    <h1>
-      Auditoria
-    </h1>
-
-
-    Olá teste
-  </div>
+    <html>
+        <h1>
+          Auditoria
+        </h1>
+        <h2>
+          Sistemas:
+        </h2>
+        <Button>
+          Trabalhos
+        </Button>
+        <br></br>
+        <Button>
+          Apontamentos
+        </Button>
+  
+  </html>
 
   );
 }
